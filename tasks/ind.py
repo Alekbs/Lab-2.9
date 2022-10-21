66af45f289aeca4a35b7acb7480989aaf22723de
Alekbs <aleksandrb436@gmail.com>
2022-10-22 00:11:46 +0300

    ind

---
+++ tasks/ind.py
@@ -0,0 +1,33 @@
+# !/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+def brackets_check(s):
+    if s:
+        for char in s:
+            if char.isalnum():
+                s = s.replace(char, '')
+        for bracket in brackets:
+            s = s.replace(bracket, '')
+        print(s)
+        if s and all([bracket not in s for bracket in brackets]):
+            return False
+
+        return brackets_check(s)
+    return True
+
+
+if __name__ == '__main__':
+    brackets = ['()', '[]']
+    if brackets_check(input('Введите строку: ')):
+        print('Скобки расставлены правильно.')
+    else:
+        print('Неправильная расстановка!')
+
+
+
+
+
+
+
+
